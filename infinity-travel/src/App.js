f9dd3b3d1606e18e74d88de4116c55b4f6c011ea
siddhkasera <smk339@scarletmail.rutgers.edu>
2023-11-13 20:13:51 -0500

    dashboard and search

--- infinity-travel/src/App.js
+++ infinity-travel/src/App.js
@@ -3,6 +3,10 @@ import React from 'react';
 import './App.css';
 import LoginForm from './components/LoginForm';
 import RegistrationForm from './components/RegistrationForm';
+import Dashboard from './components/Dashboard';
+import Search from './components/Search';
+
+
 import {
   BrowserRouter as Router,
   Routes,
@@ -12,12 +16,14 @@ import {
 function App() {
   return (
     <Router>
-      {/* <NavBar1/> */}
+
       <Routes>Home
        <Route path="/signin"  element = {<LoginForm/>}> </Route>
       <Route path ="/registration" element ={<RegistrationForm/>}></Route>
+      <Route path ="/dashboard" element ={<Dashboard/>}></Route>
+      <Route path ="/search" element ={<Search/>}></Route>
+
     </Routes>
-      {/* <Footer1/> */}
       </Router>
 
   
---
+++ infinity-travel/src/components/listItems.js
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import ListSubheader from '@mui/material/ListSubheader';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import PeopleIcon from '@mui/icons-material/People';
+import BarChartIcon from '@mui/icons-material/BarChart';
+import LayersIcon from '@mui/icons-material/Layers';
+import AssignmentIcon from '@mui/icons-material/Assignment';
+
+export const mainListItems = (
+  <React.Fragment>
+    <ListItemButton>
+      <ListItemIcon>
+        <DashboardIcon />
+      </ListItemIcon>
+      <ListItemText primary="Account" />
+    </ListItemButton>
+    <ListItemButton>
+      <ListItemIcon>
+        <ShoppingCartIcon />
+      </ListItemIcon>
+      <ListItemText primary="Home" />
+    </ListItemButton>
+    <ListItemButton>
+      <ListItemIcon>
+        <PeopleIcon />
+      </ListItemIcon>
+      <ListItemText primary="Notifications" />
+    </ListItemButton>
+    <ListItemButton>
+      <ListItemIcon>
+        <BarChartIcon />
+      </ListItemIcon>
+      <ListItemText primary="Support" />
+    </ListItemButton>
+    <ListItemButton>
+      <ListItemIcon>
+        <BarChartIcon />
+      </ListItemIcon>
+      <ListItemText primary="Logout" />
+    </ListItemButton>
+   
+  </React.Fragment>
+);
+
